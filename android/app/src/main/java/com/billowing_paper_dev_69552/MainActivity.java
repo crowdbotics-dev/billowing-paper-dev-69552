acaa912e85130e464f33ecde4369d2ca93799901
Crowdbotics <engineering@crowdbotics.com>
2023-03-15 07:43:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/billowing_paper_dev_69552/MainActivity.java
@@ -0,0 +1,15 @@
+package com.billowing_paper_dev_69552;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "billowing_paper_dev_69552";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-paper-dev-69552",
+  url: "https://billowing-paper-dev-69552.botics.co",
+  api: "https://billowing-paper-dev-69552.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
